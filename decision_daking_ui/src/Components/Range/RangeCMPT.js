a3c1483ce5d06269bedb9e0d7ba7f7ebb4a13bce
PC <saleh.majeed8@gmail.com>
2023-06-21 14:34:45 +0530

    added tooltip

--- decision_daking_ui/src/Components/Range/RangeCMPT.js
+++ decision_daking_ui/src/Components/Range/RangeCMPT.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './index.css';
 
 function RangeCMPT({ rangeValue, handleRangeChange }) {
+  const rangeStmt = [
+    'True',
+    'Mostly True',
+    'Half True',
+    'Mostly False',
+    'Half False',
+    'Pants on Fire',
+  ];
+  const [showTooltip, setShowTooltip] = useState(false);
+
   const handleChange = (event) => {
-    handleRangeChange(event.target.value);
+    const { value } = event.target;
+    handleRangeChange(value);
   };
+
+  const handleMouseEnter = () => {
+    setShowTooltip(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowTooltip(false);
+  };
+
   return (
-    <div>
+    <div style={{ position: 'relative' }}>
       <input
         type='range'
         step={1}
         min={0}
-        max={7}
+        max={5}
         value={rangeValue}
-        onChange={handleChange}
+        onInput={handleChange}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
       />
+      {showTooltip && <span className='tooltip'>{rangeStmt[rangeValue]}</span>}
     </div>
   );
 }
