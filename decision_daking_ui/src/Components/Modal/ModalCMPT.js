742a48d389b7e64ca8a300ea9b805b212dde9c27
PC <saleh.majeed8@gmail.com>
2023-06-21 14:38:11 +0530

    fixed bugs

--- decision_daking_ui/src/Components/Modal/ModalCMPT.js
+++ decision_daking_ui/src/Components/Modal/ModalCMPT.js
@@ -9,28 +9,34 @@ export default function ModalCMPT({
   setShowModal,
 }) {
   return (
-      <Modal
-        isOpen={showModal}
-        contentLabel='Modal'
-        className='modal'
-        overlayClassName='modal-overlay'
-        appElement={document.getElementById('root')}
-      >
-        {userResponses.map((eachResponse) => {
-          return (
-            <div id="massages">
-              <h1 class="firstChildOfMassages"><b>Actual Text</b> : {eachResponse.statement}</h1>
-              <h1><b>Modified Text</b> : {eachResponse.text}</h1>
-              <h1><b>Rating</b> : {eachResponse.range}</h1>
-            </div>
-          );
-        })}
-        <div class="btns">
+    <Modal
+      isOpen={showModal}
+      contentLabel='Modal'
+      className='modal'
+      overlayClassName='modal-overlay'
+      appElement={document.getElementById('root')}
+    >
+      {userResponses.map((eachResponse, index) => {
+        return (
+          <div key={index} id='massages'>
+            <h1 className='firstChildOfMassages'>
+              <b>Actual Text</b> : {eachResponse.statement}
+            </h1>
+            <h1>
+              <b>Modified Text</b> : {eachResponse.text}
+            </h1>
+            <h1>
+              <b>Rating</b> : {eachResponse.range}
+            </h1>
+          </div>
+        );
+      })}
+      <div className='btns'>
         <button onClick={() => setIsSubmit(true) && console.log(userResponses)}>
           submit
         </button>
         <button onClick={() => setShowModal(false)}>Close</button>
-        </div>
-      </Modal>
+      </div>
+    </Modal>
   );
 }
