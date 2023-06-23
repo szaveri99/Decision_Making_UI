eab99329d0509aefc7105d5b17c8b3f9e2be919c
PC <saleh.majeed8@gmail.com>
2023-06-24 04:44:22 +0530

    added new component to verify data

--- decision_daking_ui/src/App.js
+++ decision_daking_ui/src/App.js
@@ -1,107 +1,22 @@
-import './index.css';
-import QuestionCMPT from './Components/LoadQuestion';
-import BtnCMPT from './Components/Button';
-import ModalCMPT from './Components/Modal';
-import dummyData from './Dummy.js';
 import { useEffect, useState } from 'react';
+import './index.css';
+
+import ShowQuestionsCMPT from './Components/ShowQuestions/ShowQuestionsCMPT';
 
 function App() {
-  const rangeStmt = [
-    'True',
-    'Mostly True',
-    'Half True',
-    'Mostly False',
-    'Half False',
-    'Pants on Fire',
-  ];
-  const [isSubmit, setIsSubmit] = useState(false);
-  const [showModal, setShowModal] = useState(false);
-  const [questionState, setQuestionState] = useState(0);
   const [questionDataState, setquestionDataState] = useState([{}]);
-  const currentQuestion = questionDataState[questionState].text;
-  const [userResponses, setUserResponses] = useState(
-    currentQuestion.map((data) => ({
-      statement: data.text,
-      range: 0,
-      rangeTxt: rangeStmt[0],
-      text: 'N/A',
-    }))
-  );
-  const handleRangeChange = (index, newRangeValue) => {
-    console.log(index);
-    setUserResponses((prevState) => {
-      const newResponses = [...prevState];
-      newResponses[index].range = newRangeValue;
-      newResponses[index].rangeTxt = rangeStmt[newRangeValue];
-      return newResponses;
-    });
-  };
-
-  const handleTextChange = (index, newTextValue) => {
-    setUserResponses((prevState) => {
-      const newResponses = [...prevState];
-      newResponses[index].text = newTextValue;
-      return newResponses;
-    });
-  };
-
   useEffect(() => {
-    console.log(userResponses);
     const getData = async () => {
       const dataRequest = await fetch('http://localhost:5000/get-csv');
       const data = await dataRequest.json();
-      console.log(data);
-      setquestionDataState(data)
+      setquestionDataState(data);
     };
     getData();
-  }, [isSubmit]);
-
+  }, []);
+  if (questionDataState.length < 2) return;
   return (
     <>
-      <QuestionCMPT
-        data={currentQuestion}
-        index={questionState}
-        handleRangeChange={handleRangeChange}
-        handleTextChange={handleTextChange}
-        userResponse={userResponses[questionState]}
-      />
-      <div id='btn-component'>
-        <BtnCMPT
-          buttonID={'previous-btn'}
-          buttonName={'previous'}
-          handleBtnClick={() =>
-            setQuestionState((previousState) => Math.max(0, previousState - 1))
-          }
-          shouldDisable={questionState === 0}
-        />
-        <BtnCMPT
-          buttonID={'next-btn'}
-          buttonName={'next'}
-          handleBtnClick={() =>
-            setQuestionState((previousState) =>
-              Math.min(dummyData.length - 1, previousState + 1)
-            )
-          }
-          shouldDisable={questionState === dummyData.length - 1}
-        />
-        <BtnCMPT
-          buttonID={'submit-btn'}
-          buttonName={'submit'}
-          shouldDisable={questionState !== dummyData.length - 1}
-          handleBtnClick={() => setShowModal(true)}
-        />
-      </div>
-      <div id='page-number'>
-        {questionState + 1}/{dummyData.length}
-      </div>
-      {showModal && (
-        <ModalCMPT
-          showModal={showModal}
-          userResponses={userResponses}
-          setIsSubmit={setIsSubmit}
-          setShowModal={setShowModal}
-        />
-      )}
+      <ShowQuestionsCMPT questionDataState={questionDataState} />
     </>
   );
 }
---
+++ decision_daking_ui/src/Components/ShowQuestions/ShowQuestionsCMPT.js
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from 'react';
+import QuestionCMPT from '../LoadQuestion';
+import BtnCMPT from '../Button';
+import ModalCMPT from '../Modal';
+
+export default function ShowQuestionsCMPT({ questionDataState }) {
+  const rangeStmt = [
+    'True',
+    'Mostly True',
+    'Half True',
+    'Mostly False',
+    'Half False',
+    'Pants on Fire',
+  ];
+  const [isSubmit, setIsSubmit] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [questionState, setQuestionState] = useState(0);
+  const currentQuestion = questionDataState[questionState].text;
+  const [userResponses, setUserResponses] = useState(
+    questionDataState.map((data) => ({
+      statement: data.text,
+      range: 0,
+      rangeTxt: rangeStmt[0],
+      text: 'N/A',
+    }))
+  );
+  const handleRangeChange = (index, newRangeValue) => {
+    setUserResponses((prevState) => {
+      const newResponses = [...prevState];
+      newResponses[index].range = newRangeValue;
+      newResponses[index].rangeTxt = rangeStmt[newRangeValue];
+      return newResponses;
+    });
+  };
+
+  const handleTextChange = (index, newTextValue) => {
+    setUserResponses((prevState) => {
+      const newResponses = [...prevState];
+      newResponses[index].text = newTextValue;
+      return newResponses;
+    });
+  };
+
+  useEffect(() => {
+    console.log(userResponses[questionState]);
+  }, [isSubmit]);
+
+  return (
+    <>
+      {questionDataState.length > 1 && (
+        <div>
+          <QuestionCMPT
+            data={currentQuestion}
+            index={questionState}
+            handleRangeChange={handleRangeChange}
+            handleTextChange={handleTextChange}
+            userResponse={userResponses[questionState]}
+          />
+          <div id='btn-component'>
+            <BtnCMPT
+              buttonID={'previous-btn'}
+              buttonName={'previous'}
+              handleBtnClick={() =>
+                setQuestionState((previousState) =>
+                  Math.max(0, previousState - 1)
+                )
+              }
+              shouldDisable={questionState === 0}
+            />
+            <BtnCMPT
+              buttonID={'next-btn'}
+              buttonName={'next'}
+              handleBtnClick={() =>
+                setQuestionState((previousState) =>
+                  Math.min(questionDataState.length - 1, previousState + 1)
+                )
+              }
+              shouldDisable={questionState === questionDataState.length - 1}
+            />
+            <BtnCMPT
+              buttonID={'submit-btn'}
+              buttonName={'submit'}
+              shouldDisable={questionState !== questionDataState.length - 1}
+              handleBtnClick={() => setShowModal(true)}
+            />
+          </div>
+          <div id='page-number'>
+            {questionState + 1}/{questionDataState.length}
+          </div>
+          {showModal && (
+            <ModalCMPT
+              showModal={showModal}
+              userResponses={userResponses}
+              setIsSubmit={setIsSubmit}
+              setShowModal={setShowModal}
+            />
+          )}
+        </div>
+      )}
+    </>
+  );
+}
