192e91fc2053cb5a4fd7a33df9b86fa7b18dfb02
szaveri99 <sakinazaveri0609@gmail.com>
2023-06-30 13:19:54 +0100

    fetch-data api code added

--- decision_daking_ui/src/Components/Modal/ModalCMPT.js
+++ decision_daking_ui/src/Components/Modal/ModalCMPT.js
@@ -1,6 +1,9 @@
 import './index.css';
+import axios from 'axios';
 import React from 'react';
 import Modal from 'react-modal';
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 
 export default function ModalCMPT({
   showModal,
@@ -8,7 +11,27 @@ export default function ModalCMPT({
   setIsSubmit,
   setShowModal,
 }) {
+
+  const submitData = async (data) => {
+    try {
+     
+      const formattedData = data.map(({ statement, text, rangeTxt }) => ({
+        statement,
+        text,
+        rangeTxt,
+      }));
+
+      const response = await axios.post('http://localhost:5000/fetch-data', formattedData);
+      console.log(response.formattedData);
+      // toast.success('Data submitted successfully');
+    } catch (error) {
+      console.error(error);
+      // toast.error('Failed to submit data !!');
+    }
+  };
+
   return (
+
     <Modal
       isOpen={showModal}
       contentLabel='Modal'
@@ -32,8 +55,11 @@ export default function ModalCMPT({
         );
       })}
       <div className='btns'>
-        <button onClick={() => setIsSubmit(true) && console.log(userResponses)}>
-          submit
+        <button onClick={() => {
+          setIsSubmit(true);
+          submitData(userResponses);
+          setShowModal(false)}}>
+          Submit
         </button>
         <button onClick={() => setShowModal(false)}>Close</button>
       </div>
