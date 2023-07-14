5a932f09fb7be39431bb428b310ebeafb2ac3091
szaveri99 <sakinazaveri0609@gmail.com>
2023-07-14 14:25:12 +0100

    values of likert scale list changed

--- decision_daking_ui/src/Components/LoadQuestion/QuestionCMPT.js
+++ decision_daking_ui/src/Components/LoadQuestion/QuestionCMPT.js
@@ -12,13 +12,15 @@ function QuestionCMPT({
   userResponse,
 }) {
   const [tickState, setTickState] = useState(false);
-  const currentInstructions = 'Please follow propper guide lines';
+  const currentInstructions = "To use this application, follow these steps:. 1)Start by consider the following range from True to False to interpret the statement.2)Enter the text in the provided input field to see the results that is provided by the classifier. 3)Click the Submit button to send the text to the server for classification. 4)Wait for the response to see the predicted class. Please keep in mind the following:. 1) Make sure you enter valid text that you want to classify, this will ensure accurate results. 2) Avoid submitting empty or irrelevant text, as it may affect the classification accuracy.";
+  const txt = currentInstructions.split('.').map(str => <p>{str}</p>)
+  
   useEffect(() => {
     setTickState(false);
   }, [data]);
   return (
     <>
-      <InstructionsCMPT insText={currentInstructions} />
+      <InstructionsCMPT insText={txt} />
       <StmtCMPT txt={data} />
       <RangeCMPT
         rangeValue={userResponse.range}
