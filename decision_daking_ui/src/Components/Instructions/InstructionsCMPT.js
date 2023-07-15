6168c0b3321ae59c0bde8d46a059f8aaee98f2d8
szaveri99 <sakinazaveri0609@gmail.com>
2023-07-16 00:51:44 +0100

    instruction content changed

--- decision_daking_ui/src/Components/Instructions/InstructionsCMPT.js
+++ decision_daking_ui/src/Components/Instructions/InstructionsCMPT.js
@@ -3,7 +3,7 @@ import React from 'react';
 function InstructionsCMPT({ insText }) {
   return (
     <div className='prop-guidline'>
-      To use this application, follow these steps:{' '}
+      {/* To use this application, follow these steps:{' '} */}
       {insText.split('.').map((str, currentKey) => (
         <p key={currentKey} dangerouslySetInnerHTML={{ __html: str }}></p>
       ))}
