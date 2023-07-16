6ef4b6471d2b0b0507d7f9888e8104ca6c0d0f68
szaveri99 <sakinazaveri0609@gmail.com>
2023-07-16 14:09:18 +0100

    added some print stmnts

--- decision_daking_ui/src/Components/LoadQuestion/QuestionCMPT.js
+++ decision_daking_ui/src/Components/LoadQuestion/QuestionCMPT.js
@@ -18,6 +18,8 @@ function QuestionCMPT({
     '<b>To use this application, follow these steps: <b/>. <i>1)Start by consider the following range from <b>True</b> to <b>False</b> to interpret the statement<i/>.<i>2)Enter the text in the provided input field to see the results that is provided by the classifier<i/>. <i>3)Click the Submit button to send the text to the server for classification<i/>. <i>4)Wait for the response to see the predicted class<i/>. <b>Please keep in mind the following:</b>.<i> 1) Make sure you enter valid text that you want to classify, this will ensure accurate results<i/>. <i>2) Avoid submitting empty or irrelevant text, as it may affect the classification accuracy <i/>';
   const txt = currentInstructions;
   const [classifierValue, setClassifierValue] = useState(null);
+  console.log('classifierRef:', classifierRef)
+  console.log('classifierCurrent:',classifierRef.current)
   if (classifierRef.current) {
     classifierRef.current.value = classifierValue === true ? 0 : 1;
     console.log(classifierRef.current);
