3d3ccc1ee571bd29255d38f66e9fd0dcaf49854c
szaveri99 <sakinazaveri0609@gmail.com>
2023-07-30 16:00:18 +0100

    disabled the range slider

--- decision_daking_ui/src/Components/Classifier/ClassifierCMPT.js
+++ decision_daking_ui/src/Components/Classifier/ClassifierCMPT.js
@@ -8,12 +8,14 @@ export default function ClassifierCMPT({ classifierValue }) {
         True{' '}
         <input
           type='range'
+          id = 'range-color'
           step={1}
           min={0}
           max={1}
           className={classifierValue === 'True' ? 'green' : 'red'}
           defaultValue={classifierValue === 'True' ? 0 : 1}
           readOnly
+          disabled
         />{' '}
         False
         <div className='classifier-text'>Classifier Detection</div>
