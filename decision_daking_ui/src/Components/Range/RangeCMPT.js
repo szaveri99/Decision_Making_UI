e42bd2f6bf7e503c7da0483a257518db7d5c3975
PC <saleh.majeed8@gmail.com>
2023-08-05 18:56:28 +0530

    added range slider

--- decision_daking_ui/src/Components/Range/RangeCMPT.js
+++ decision_daking_ui/src/Components/Range/RangeCMPT.js
@@ -1,33 +1,69 @@
 import React from 'react';
-import Range from 'rc-slider';
-import 'rc-slider/assets/index.css';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
 import './index.css';
 
-function RangeCMPT({ rangeValue, handleRangeChange, index }) {
-  const rangeStmt = [
-    'true',
-    'mostly-true',
-    'half-true',
-    'barely-false',
-    'false',
-    'pants-on-fire',
-  ];
+const marks = [
+  {
+    value: 0,
+    label: 'true',
+  },
+  {
+    value: 1,
+    label: 'mostly-true',
+  },
+  {
+    value: 2,
+    label: 'half-true',
+  },
+  {
+    value: 3,
+    label: 'barely-false',
+  },
+  {
+    value: 4,
+    label: 'false',
+  },
+  {
+    value: 5,
+    label: 'pants-on-fire',
+  },
+];
 
+function valuetext(value) {
+  const rangeValue = marks[value].label;
+  console.log(rangeValue);
+  return rangeValue;
+}
+
+function RangeCMPT({ rangeValue, handleRangeChange, index }) {
   const handleChange = (event) => {
-    // const { value } = event.target;
-    handleRangeChange(index, event);
+    handleRangeChange(index, event.target.value);
   };
 
   return (
     <div style={{ position: 'relative' }}>
-      True <Range
+      {/* True <Range
         step={1}
         min={0}
         max={5}
         defaultValue={0}
         ariaLabelForHandle={'hello'}
         onChange={handleChange}
-      /> False
+      /> False */}
+      <Box sx={{ width: 300 }}>
+        <Slider
+          step={1}
+          min={0}
+          max={5}
+          defaultValue={0}
+          aria-label='Custom marks'
+          getAriaValueText={valuetext}
+          valueLabelDisplay='auto'
+          marks={marks}
+          onChange={handleChange}
+        />
+      </Box>
     </div>
   );
 }
