44d38ea0581c18d03cb8670a372dcee95702d374
szaveri99 <sakinazaveri0609@gmail.com>
2023-08-12 15:41:24 +0100

    range slider style added

--- decision_daking_ui/src/Components/Range/RangeCMPT.js
+++ decision_daking_ui/src/Components/Range/RangeCMPT.js
@@ -51,7 +51,7 @@ function RangeCMPT({ rangeValue, handleRangeChange, index }) {
         ariaLabelForHandle={'hello'}
         onChange={handleChange}
       /> False */}
-      <Box sx={{ width: 300 }}>
+      <Box sx={{ width: 600 }}>
         <Slider
           step={1}
           min={0}
@@ -62,6 +62,24 @@ function RangeCMPT({ rangeValue, handleRangeChange, index }) {
           valueLabelDisplay='auto'
           marks={marks}
           onChange={handleChange}
+          colors={['#FF5722', '#FF9800']}
+          sx={{
+            width: 600,
+            height: 10,
+            color: 'white', // Hide the default track color
+            '& .MuiSlider-rail': {
+              backgroundImage: 'linear-gradient(to right, #42B3F5, #609CD2, #7E86AE, #9C6F8B, #BA5967, #D84244);',
+              height: 10,
+            },
+            '& .MuiSlider-thumb': {
+              backgroundColor: 'white', // Customize thumb color
+            },
+            '& .MuiSlider-markLabel': {
+              color: 'white', // Change marks text color
+              fontWeight: 'bold',
+              fontSize: 14,
+            },
+          }}
         />
       </Box>
     </div>
