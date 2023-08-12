d525d3f216eba644fe7e587886be296c39ab4c9d
szaveri99 <sakinazaveri0609@gmail.com>
2023-08-12 15:43:23 +0100

    Submit spelling corrected

--- decision_daking_ui/src/Components/Modal/ModalCMPT.js
+++ decision_daking_ui/src/Components/Modal/ModalCMPT.js
@@ -41,7 +41,7 @@ export default function ModalCMPT({
         });
       })}
       <div className='btns'>
-        <button onClick={() => handleSubmit()}>submit</button>
+        <button onClick={() => handleSubmit()}>Submit</button>
         <button onClick={() => setShowModal(false)}>Close</button>
       </div>
     </Modal>
